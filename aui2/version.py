b9ae38568dd3eff7fa5a3c8dc3064f6a2b192704
Tianzhu Qiao <tq@feiyilin.com>
2024-03-10 21:02:34 -0700

    ver 0.1.2

--- aui2/version.py
+++ aui2/version.py
@@ -2,8 +2,8 @@ PROJECT_NAME     = 'aui2'
 
 VER_MAJOR        = 0
 VER_MINOR        = 1
-VER_RELEASE      = 1
+VER_RELEASE      = 2
 
-VER_FLAGS        = "a1"
+VER_FLAGS        = ""
 
 __version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'
